ca2de59d7713ab9214e470bba172f7c630bff78d
Anthony FS <anthonyfs@Anthonys-MBP.attlocal.net>
2022-08-07 10:14:31 -0700

    refactored funtions to single line return

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 
-const add = (num1, num2) => {
-    return num1 + num2
-}
+const add = (num1, num2) => num1 + num2
 
-const subtract = (num1, num2) => {
-    return num1 - num2
-}
+const subtract = (num1, num2) => num1 - num2
 
-const multiply = (num1, num2) => {
-    return num1 * num2
-}
+const multiply = (num1, num2) => num1 * num2
 
-const divide = (num1, num2) => {
-    return num1 / num2
-}
+const divide = (num1, num2) => num1 / num2
 
-const squareRoot = (num) => {
-    return Math.sqrt(num)
-}
+const squareRoot = (num) => Math.sqrt(num)
 
-const max = (num1, num2) => {
-    return Math.max(num1, num2)
-}
+const max = (num1, num2) => Math.max(num1, num2)
 
 
 module.exports = {
